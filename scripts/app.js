25e1f3941423f629697bf66cf19a235f3d8a26ca
akrasny <alexander.a.krasny@gmail.com>
2015-05-18 18:28:35 +0300

    in progress

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+angular.module('MySpace', []);
+
+angular.module('MySpace').controller('TestCtrl', function($scope) {
+    $scope.hello = 'Hello, Angular!';
+    $scope.table1 = 'Hello, Table1!';
+});
+
+angular.module('MySpace').controller('TestSidebarCtrl', function ($scope) {
+    $scope.sidebar = 'Hello, Sidebar!';
+});
+
+angular.module('MySpace').controller('TableCtrl', function ($scope) {
+    $scope.table = 'Hello, Table!';
+});
